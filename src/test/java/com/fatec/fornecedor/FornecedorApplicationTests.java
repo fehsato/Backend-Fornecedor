3648bd391f808dbb48e33f852c0cf0a440313bfd
fehsato <fyonezawa14@gmail.com>
2023-06-13 15:38:06 -0300

    pronto2

--- src/test/java/com/fatec/fornecedor/FornecedorApplicationTests.java
+++ src/test/java/com/fatec/fornecedor/FornecedorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fatec.client;
+package com.fatec.fornecedor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClientApplicationTests {
+class FornecedorApplicationTests {
 
 	@Test
 	void contextLoads() {
